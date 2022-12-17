063bab7a2780a2d03d92d4b6dbd764b49243e299
harsh <93788928+harsh23079@users.noreply.github.com>
2022-12-17 09:26:44 +0530

    git help submodule

---
+++ myfirstproject/myfirstproject/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  myfirstproject
+//
+//  Created by Harsh Mishra on 19/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("ding_ding_1")
+            Text("abhishek yadav")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myfirstproject/myfirstproject/myfirstprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfirstprojectApp.swift
+//  myfirstproject
+//
+//  Created by Harsh Mishra on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct myfirstprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
